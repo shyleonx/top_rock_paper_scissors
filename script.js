3ca46e2debb8905f089e062fb5e21e58c38dabab
shyleonx <shyleon.x@gmail.com>
2024-02-05 17:41:49 +0800

    modified functions
    remove the playerChoice and userChoice const variables.
    
    add new getUserChoice function that will take an input from a user

--- script.js
+++ script.js
@@ -13,24 +13,53 @@ function getComputerChoice() {
     }
 }
 
+/* I ADD ANOTHER FUNCTION FOR USER CHOICE SO IT'S MORE CLEAN */
+
+function getUserChoice() {
+    const userChoice = prompt("Choose: Rock, Paper, Scissor: ").toLowerCase();
+
+    if (userChoice === "rock" || userChoice === "paper" || userChoice ==="scissor") {
+        return userChoice;
+    } else {
+        alert("You can only choose between rock, paper. or scissor")
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         console.log("it's a tie");
     } else if (playerSelection === "rock" && computerSelection === "scissor") {
         console.log("You win! Rock crushes scissor");
+        playerScore++;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("You win! Paper covers rock");
+        playerScore++;
     } else if (playerSelection === "scissor" && computerSelection === "paper") {
         console.log("You win! Scissor cuts paper");
+        playerScore++;
     } else {
         console.log("You lose :(");
+        computerScore++;
     }
-
+    console.log("Your score: " + playerScore);
+    console.log("Computer score: " + computerScore);
 }
 
+function playGame() {
+    while (playerScore < 5 && computerScore < 5) {
+        const playerSelection = getUserChoice();
+        const computerSelection = getComputerChoice();
 
-const playerSelection = prompt("Choose Rock, Paper, Scissor: ").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-playRound(playerSelection, computerSelection);
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+    }
+
+    if (playerScore >= 5) {
+        console.log("You won the race to 5!");
+    } else {
+        console.log("You lost");
+    }
+}
+
+playGame();
